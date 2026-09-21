6e472bbea3125a1e58f2a42b7bbe92886df0cb98
wojciech <wojciech@user.pl>
2026-09-21 20:06:09 +0200

    last changes

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -8,6 +8,6 @@ public class ColorSupplier {
     public String getRandomColor() {
         Color[] colors = Color.values();
         int value = randomColor.nextInt(colors.length);
-        return colors[value].name().toLowerCase();
+        return colors[value].name();
     }
 }
--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,13 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
-    public static final int FIGURE_COUNT = FigureNames.values().length;
-    public static final int RANDOM_LIMIT = 50;
-    public static final int DEFAULT_RADIUS = 10;
-    public static final String DEFAULT_COLOR = "white";
+    private static final int FIGURE_COUNT = FigureNames.values().length;
+    private static final int RANDOM_LIMIT = 50;
+    private static final int DEFAULT_RADIUS = 10;
+    private static final String DEFAULT_COLOR = Color.WHITE.name();
 
-    private Random randomValueSupplier = new Random();
-    private ColorSupplier colorSupplier = new ColorSupplier();
+    private final Random randomValueSupplier = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Figure getDefaultFigure() {
         return new Circle(DEFAULT_RADIUS, DEFAULT_COLOR);
@@ -19,30 +19,32 @@ public class FigureSupplier {
         int figureIndex = randomValueSupplier.nextInt(FIGURE_COUNT);
         FigureNames figureName = FigureNames.values()[figureIndex];
         String color = colorSupplier.getRandomColor();
-        if (figureName == FigureNames.CIRCLE) {
-            int radius = randomValueSupplier.nextInt(RANDOM_LIMIT) + 1;
-            return new Circle(radius, color);
-        } else if (figureName == FigureNames.SQUARE) {
-            int side = randomValueSupplier.nextInt(RANDOM_LIMIT) + 1;
-            return new Square(side, color);
-        } else if (figureName == FigureNames.RECTANGLE) {
-            int sideA = randomValueSupplier.nextInt(RANDOM_LIMIT) + 1;
-            int sideB = randomValueSupplier.nextInt(RANDOM_LIMIT) + 1;
-            return new Rectangle(sideA, sideB, color);
-        } else if (figureName == FigureNames.RIGHT_TRIANGLE) {
-            int firstLeg = randomValueSupplier.nextInt(RANDOM_LIMIT) + 1;
-            int secondLeg = randomValueSupplier.nextInt(RANDOM_LIMIT) + 1;
-            return new RightTriangle(firstLeg,
+        switch (figureName) {
+            case CIRCLE:
+                int radius = randomValueSupplier.nextInt(RANDOM_LIMIT) + 1;
+                return new Circle(radius, color);
+            case SQUARE:
+                int side = randomValueSupplier.nextInt(RANDOM_LIMIT) + 1;
+                return new Square(side, color);
+            case RECTANGLE:
+                int sideA = randomValueSupplier.nextInt(RANDOM_LIMIT) + 1;
+                int sideB = randomValueSupplier.nextInt(RANDOM_LIMIT) + 1;
+                return new Rectangle(sideA, sideB, color);
+            case RIGHT_TRIANGLE:
+                int firstLeg = randomValueSupplier.nextInt(RANDOM_LIMIT) + 1;
+                int secondLeg = randomValueSupplier.nextInt(RANDOM_LIMIT) + 1;
+                return new RightTriangle(firstLeg,
                     secondLeg,
                     color);
-        } else {
-            double topBase = randomValueSupplier.nextInt(RANDOM_LIMIT) + 1;
-            double bottomBase = randomValueSupplier.nextInt(RANDOM_LIMIT) + 1;
-            double height = randomValueSupplier.nextInt(RANDOM_LIMIT) + 1;
-            return new IsoscelesTrapezoid(topBase,
+            case ISOSCELES_TRAPEZOID: 
+                double topBase = randomValueSupplier.nextInt(RANDOM_LIMIT) + 1;
+                double bottomBase = randomValueSupplier.nextInt(RANDOM_LIMIT) + 1;
+                double height = randomValueSupplier.nextInt(RANDOM_LIMIT) + 1;
+                return new IsoscelesTrapezoid(topBase,
                     bottomBase,
                     height,
                     color);
+            default: return null; 
         }
     }
 }
