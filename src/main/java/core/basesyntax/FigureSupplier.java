deb823770859cf1281340d33d2955e2e80710b79
wojciech <wojciech@user.pl>
2026-09-21 14:10:46 +0200

    Changes

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,14 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
-
-    public static final int FIGURE_COUNT = 5;
+    public static final int FIGURE_COUNT = FigureNames.values().length;
     public static final int RANDOM_LIMIT = 50;
     public static final int DEFAULT_RADIUS = 10;
     public static final String DEFAULT_COLOR = "white";
 
     private Random randomValueSupplier = new Random();
-    private ColorSupplier color = new ColorSupplier();
+    private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Figure getDefaultFigure() {
         return new Circle(DEFAULT_RADIUS, DEFAULT_COLOR);
@@ -18,36 +17,34 @@ public class FigureSupplier {
 
     public Figure getRandomFigure() {
         int figureIndex = randomValueSupplier.nextInt(FIGURE_COUNT);
-        String figureName = FigureNames.values()[figureIndex].toString();
+        String figureName = FigureNames.values()[figureIndex].name();
+        String color = colorSupplier.getRandomColor();
         switch (figureName) {
             case "CIRCLE":
                 int radius = randomValueSupplier.nextInt(RANDOM_LIMIT) + 1;
-                return new Circle(radius, color.getRandomColor());
+                return new Circle(radius, color);
             case "SQUARE":
-                int sideA = randomValueSupplier.nextInt(RANDOM_LIMIT) + 1;
-                return new Square(sideA, color.getRandomColor());
+                int sideAB = randomValueSupplier.nextInt(RANDOM_LIMIT) + 1;
+                return new Square(sideAB, color);
             case "RECTANGLE":
-                sideA = randomValueSupplier.nextInt(RANDOM_LIMIT) + 1;
+                int sideA = randomValueSupplier.nextInt(RANDOM_LIMIT) + 1;
                 int sideB = randomValueSupplier.nextInt(RANDOM_LIMIT) + 1;
-                Figure rectangle = new Rectangle(sideA, sideB, color.getRandomColor());
-                return rectangle;
+                return new Rectangle(sideA, sideB, color);
             case "RIGHT_TRIANGLE":
                 int firstLeg = randomValueSupplier.nextInt(RANDOM_LIMIT) + 1;
                 int secondLeg = randomValueSupplier.nextInt(RANDOM_LIMIT) + 1;
-                Figure rightTriangle = new RightTriangle(firstLeg,
+                return new RightTriangle(firstLeg,
                         secondLeg,
-                        color.getRandomColor());
-                return rightTriangle;
+                        color);
             default:
             case "ISOSCELES_TRAPEZOID":
                 double topBase = randomValueSupplier.nextInt(RANDOM_LIMIT) + 1;
                 double bottomBase = randomValueSupplier.nextInt(RANDOM_LIMIT) + 1;
                 double height = randomValueSupplier.nextInt(RANDOM_LIMIT) + 1;
-                Figure isoscelesTrapezoid = new IsoscelesTrapezoid(topBase,
+                return new IsoscelesTrapezoid(topBase,
                         bottomBase,
                         height,
-                        color.getRandomColor());
-                return isoscelesTrapezoid;
+                        color);
         }
     }
 }
